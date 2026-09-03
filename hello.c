327f6fb0cbd514a529216ba87b345d87b46eec1c
Prince-9x08 <prince18900.2@gmail.com>
2026-09-03 20:44:15 +0530

    added a area of sq. finder

--- hello.c
+++ hello.c
@@ -1,5 +1,37 @@
-# include<stdio.h>
+// # include<stdio.h>
+// int main(){
+//     char name[50];
+//     printf("enter name :");
+//     scanf("%s",name);
+//     printf("name is : %s",name);
+//     return 0;
+// }
+
+// #include <stdio.h>
+
+// int main(){
+//     int a,b;
+//     printf("enter the num a:");
+//     scanf("%d",&a);
+
+//     printf("enter the num b:");
+//     scanf("%d",&b);
+    
+//     int sum= a+b;
+//     printf("the sum of a+b=%d",sum);
+//     return 0;
+// }
+
+/* ====making a program to find side of square===*/
+
+#include <stdio.h>
+
 int main(){
-    printf("hello world");
-    return(0);
+    float side;
+    printf("enter the side of square:");
+    scanf("%f",&side);
+
+    printf("area of square = %fcm^2",side * side);
+    return 0;
+
 }
