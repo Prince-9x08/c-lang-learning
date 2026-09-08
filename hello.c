001cd9d34672e3138f624b4d57608c43c2d7b6f3
Prince-9x08 <prince18900.2@gmail.com>
2026-09-08 21:14:32 +0530

    done some calculation

--- hello.c
+++ hello.c
@@ -55,20 +55,26 @@ int main(){
 //     return 0;
 // }
 
+// #include <stdio.h>
+// int main(){
+//     int a=10;
+//     int b=20;
+
+//     int sum= a + b;
+//     int sub= a - b;
+//     int product= a * b;
+//     int div= a/b;
+
+//     printf("sum of a + b = %d\n",sum);
+//     printf("sub of a - b = %d\n",sub);
+//     printf("prod of a * b = %d\n",product);
+//     printf("div of a / b = %d\n",div);
+//     return 0;
+// }
+
 #include <stdio.h>
 int main(){
-    int a=10;
-    int b=20;
-
-    int sum= a + b;
-    int sub= a - b;
-    int product= a * b;
-    int div= a/b;
-
-    printf("sum of a + b = %d\n",sum);
-    printf("sub of a - b = %d\n",sub);
-    printf("prod of a * b = %d\n",product);
-    printf("div of a / b = %d\n",div);
-    return 0;
+    int a=5+2/2*3;
+    printf("value of a=%d",a);
+    return 0 ;
 }
-
